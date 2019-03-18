9e12d12dbeca0d6a1240392db7019de176c625ba
tyttpe <994148249@qq.com>
2019-03-18 13:11:57 +0800

    spring boot controller

---
+++ spring-boot-study/spring-boot-mybatis/src/main/java/com/springboot/mybatis/mapper/CourseMapper.java
@@ -0,0 +1,39 @@
+package com.springboot.mybatis.mapper;
+
+import com.springboot.mybatis.entity.Course;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+
+public interface CourseMapper{
+    @Results({@Result(column = "course_id",property = "courseId"),
+            @Result(column = "course_name",property = "courseName"),
+            @Result(column = "user_id",property = "userId"),
+            @Result(column = "course_class",property = "courseClass"),
+            @Result(column = "cover",property = "cover"),
+            @Result(column = "course_code",property = "courseCode"),
+            @Result(column = "finished",property = "finished")
+    })
+
+    @Select("SELECT * FROM t_course ")
+    List<Course> selectAll();
+
+    @Results({@Result(column = "course_id",property = "courseId"),
+            @Result(column = "course_name",property = "courseName"),
+            @Result(column = "user_id",property = "userId"),
+            @Result(column = "course_class",property = "courseClass"),
+            @Result(column = "cover",property = "cover"),
+            @Result(column = "course_code",property = "courseCode"),
+            @Result(column = "finished",property = "finished")
+    })
+
+    @Select("SELECT * FROM t_course WHERE course_id = #{courseId} ")
+    Course getOne(Long courseId);
+
+    @Delete("DELETE FROM t_course WHERE course_id =#{courseId} ")
+    void delete(Long courseId);
+}
